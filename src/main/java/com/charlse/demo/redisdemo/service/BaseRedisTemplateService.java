e1c544692e9a34d33b952e6ea7e202b90073bc71
ChenJun <junchen.time@foxmail.com>
2020-06-03 20:37:03 +0800

    熟悉spring.redis的配置文件url属性，加上了auth

--- src/main/java/com/charlse/demo/redisdemo/service/BaseRedisTemplateService.java
+++ src/main/java/com/charlse/demo/redisdemo/service/BaseRedisTemplateService.java
@@ -1,9 +1,11 @@
 package com.charlse.demo.redisdemo.service;
 
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,13 +14,14 @@ public class BaseRedisTemplateService {
     private static final Logger logger = LoggerFactory.getLogger(BaseRedisTemplateService.class);
 
     @Autowired
-    private RedisTemplate<String, String> redisTemplate;
+    private RedisTemplate redisTemplate;
 
     public void addValue(String key, String value){
         redisTemplate.opsForValue().append(key, value);
     }
 
     public String getValue(String key){
-        return redisTemplate.opsForValue().get(key);
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate.opsForValue().get(key).toString();
     }
 }
